787f91ac2a096764e2c5bdffa7972fc7405c6daa
Albert Unmack Bærentzen <202104567@post.au.dk>
2025-12-25 16:53:57 +0100

    Added logging and updating when player playing a character leaves. Resolves #5

---
+++ events/guildMemberRemove.js
@@ -0,0 +1,26 @@
+const { Events } = require('discord.js');
+const { Deceased, Players, Characters } = require('../dbObjects.js');
+const { postInLogChannel, COLORS, addDeceasedToDatabase } = require('../misc');
+
+module.exports = {
+  name: Events.GuildMemberRemove,
+  async execute(member) {
+    // When a member leaves, then if they were playing a character, note it in
+    // the storyteller log and set them as inactive in the database.
+    const player = await Players.findByPk(member.id);
+    if (player) {
+      const character = await player.getCharacter();
+      if (character) {
+        await postInLogChannel(
+          'Player Left Server',
+          `Player **${member.user.tag}** (ID: ${member.id}) has left the server.\n\n` +
+          `They were playing the character **${character.name}** (ID: ${character.id}).`,
+          COLORS.RED
+        );
+        // Set character as deceased
+        await addDeceasedToDatabase(client.application.id, false, { characterId: character.id, yearOfDeath: world.currentYear, monthOfDeath: 1, dayOfDeath: 1, causeOfDeath: 'Left the continent', playedById: player.id })
+      }
+    }
+
+  }
+};
