a0967d37b591e0022da1d2c6fdfbd946ecd526bb
Albert Unmack Bærentzen <202104567@post.au.dk>
2026-04-07 15:36:30 +0200

    Added new model

---
+++ models/LegitimisationRequests.js
@@ -0,0 +1,22 @@
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('legitimisationrequests', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      unique: true,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    offspringId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+    },
+    requestedById: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    newName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    }
+  });
+}
