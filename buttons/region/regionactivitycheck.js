e1c2eb6e602a8cb511e1ff7295ee17f3bd29a38f
BadgerGM <brstubbs01@gmail.com>
2026-04-09 22:48:03 +1000

    Added simple check button to send to channel

--- buttons/region/regionactivitycheck.js
+++ buttons/region/regionactivitycheck.js
@@ -18,29 +18,22 @@ module.exports = {
                     `In this menu, you can conduct an activity check on the citizens of your region, **${region.name}**. \n\n` +
                     `There are three types of activity checks you can conduct:\n` +
                     `- A **Simple Activity Check** will provide an activity check button in a selected channel that will allow citizens to check in for 48 hours. This is useful for quicker checks on whether citizens are active.\n` + 
-                    `- A **Detailed Activity Check** will provide a more detailed activity check by asking citizens to provide information on Discord about their recent activity in the region, such as their role, activity, city of residence. This is useful for getting a better understanding of how active your citizens are and what they are doing in the region.\n` +
-                    `- An **Ingame Activity Check** will allow you to conduct a more detailed activity check using in-game mechanics such as books by adding a keyword that citizens can input on discord to note ingame completion.\n`
+                    `- A **Detailed Activity Check** will provide a more detailed activity check by asking citizens to provide information on Discord about their recent activity in the region, such as their role, activity, city of residence. This is useful for getting a better understanding of how active your citizens are and what they are doing in the region.\n`
                 )
             )
             .addSeparatorComponents((separator) => separator)
             .addTextDisplayComponents((textDisplay) => textDisplay.setContent('Please select what type of check you would like to conduct.\n'));
 
         const simpleActivityButton = new ButtonBuilder()
-            .setCustomId('simple-activity-button')
+            .setCustomId('simple-activity-check-button')
             .setLabel('Simple Activity Check')
             .setEmoji('💬')
             .setStyle(ButtonStyle.Secondary);
 
         const detailedActivityButton = new ButtonBuilder()
-            .setCustomId('detailed-activity-button')
+            .setCustomId('detailed-activity-check-button')
             .setLabel('Detailed Activity Check')
             .setEmoji('✉️')
-            .setStyle(ButtonStyle.Secondary);
-
-        const ingameActivityButton = new ButtonBuilder()
-            .setCustomId('ingame-activity-button')
-            .setLabel('Ingame Activity Check')
-            .setEmoji('🌲')
             .setStyle(ButtonStyle.Secondary);
 
         const cancelButton = new ButtonBuilder()
@@ -53,7 +46,6 @@ module.exports = {
             actionRow.setComponents(
                 simpleActivityButton,
                 detailedActivityButton,
-                ingameActivityButton,
                 cancelButton
             )
         );
